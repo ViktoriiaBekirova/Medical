b99cd8886ddc549a14a00bb2a526030ad42567e7
bekirova <viktoriia.bekirova@gmail.com>
2019-01-16 20:48:20 +0300

    add services

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,100 @@
 <template>
-  <div class="main">
-    <div class="wrapper">
-      <div class="main-block">
-        <h1 class="main-block__header">We are the team of excellence</h1>
-        <p class="main-block__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore  et dolore magna aliqua.</p>
-        <button class="main-block__button">
-          <span>Get Started</span>
-        </button>
+  <div>
+    <section class="main">
+      <div class="wrapper">
+        <div class="main-block">
+          <h1 class="main-block__header">We are the team of excellence</h1>
+          <p class="main-block__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore  et dolore magna aliqua.</p>
+          <button class="main-block__button">
+            <span>Get Started</span>
+          </button>
+        </div>
       </div>
-    </div>
-    <div class="main-image">
-      <div class="main-image__img">
-        <picture>
-          <source media="(max-width: 480px)" srcset="/img/header-img.png">
-          <img class="" src="/img/header-img.png" data-src="" alt="bg">
-        </picture>
+      <div class="main-image">
+        <div class="main-image__img">
+          <picture>
+            <source media="(max-width: 480px)" srcset="/img/header-img.png">
+            <img class="" src="/img/header-img.png" data-src="" alt="bg">
+          </picture>
+        </div>
       </div>
-    </div>
+    </section>
+    <section class="services">
+      <div class="wrapper">
+        <div class="row">
+          <div class="col col--6 col-s--4">
+            <div class="services-content">
+              <div class="services-content__icon">
+                <span class="lnr lnr-rocket"></span>
+              </div>
+              <div class="services-content__text">
+                <h4>24/7 emergency</h4>
+                <p>
+                  inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct women face higher conduct.
+                </p>
+              </div>
+            </div>
+            <div class="services-content">
+              <div class="services-content__icon">
+                <span class="lnr lnr-chart-bars"></span>
+              </div>
+              <div class="services-content__text">
+                <h4>2X-Ray Service</h4>
+                <p>
+                  inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct women face higher conduct.
+                </p>
+              </div>
+            </div>
+            <div class="services-content">
+              <div class="services-content__icon">
+                <span class="lnr lnr-bug"></span>
+              </div>
+              <div class="services-content__text">
+                <h4>Intensive Care</h4>
+                <p>
+                  inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct women face higher conduct.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col col--6 col-s--4">
+            <div class="services-content">
+              <div class="services-content__icon">
+                <span class="lnr lnr-heart-pulse"></span>
+              </div>
+              <div class="services-content__text">
+                <h4>24/7 emergency</h4>
+                <p>
+                  inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct women face higher conduct.
+                </p>
+              </div>
+            </div>
+            <div class="services-content">
+              <div class="services-content__icon">
+                <span class="lnr lnr-paw"></span>
+              </div>
+              <div class="services-content__text">
+                <h4>2X-Ray Service</h4>
+                <p>
+                  inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct women face higher conduct.
+                </p>
+              </div>
+            </div>
+            <div class="services-content">
+              <div class="services-content__icon">
+                <span class="lnr lnr-bug"></span>
+              </div>
+              <div class="services-content__text">
+                <h4>Intensive Care</h4>
+                <p>
+                  inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct women face higher conduct.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
