c2c0ba7d8d29e3d21809227123edaf2ce97cd0e5
bekirova <viktoriia.bekirova@gmail.com>
2019-01-16 17:25:33 +0300

    add main block

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="main">
-
+    <div class="wrapper">
+      <div class="main-block">
+        <h1 class="main-block__header">We are the team of excellence</h1>
+        <p class="main-block__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore  et dolore magna aliqua.</p>
+        <button class="main-block__button">
+          <span>Get Started</span>
+        </button>
+      </div>
+    </div>
+    <div class="main-image">
+      <div class="main-image__img">
+        <picture>
+          <source media="(max-width: 480px)" srcset="/img/header-img.png">
+          <img class="" src="/img/header-img.png" data-src="" alt="bg">
+        </picture>
+      </div>
+    </div>
   </div>
 </template>
 
